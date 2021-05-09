64fec19c51ac9321a3fcbe0a6e13c6b34f1e6f59
Łukasz Macoń <luk.macon@gmail.com>
2021-05-09 14:33:16 +0200

    Create project

---
+++ src/main/java/com/example/movieservice/movie/Movie.java
@@ -0,0 +1,34 @@
+package com.example.movieservice.movie;
+
+public class Movie {
+    private Long ID;
+    private String name;
+    private String category;
+
+    public Movie() {
+    }
+
+    public Long getID() {
+        return ID;
+    }
+
+    public void setID(Long ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
