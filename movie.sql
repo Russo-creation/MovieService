d173c8ef3c55af72675bf3f41ad570134ff7a799
Łukasz Macoń <luk.macon@gmail.com>
2021-06-03 15:39:09 +0200

    update SQL file

--- movie.sql
+++ movie.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: jaz_4_mysql_local
--- Czas generowania: 19 Maj 2021, 00:45
+-- Czas generowania: 03 Cze 2021, 13:38
 -- Wersja serwera: 5.7.33
 -- Wersja PHP: 7.4.16
 
@@ -28,20 +28,22 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `movie` (
-  `id` bigint(20) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `category` enum('SCI_FI','ANIME','FANTASY','') NOT NULL
+                         `id` bigint(20) NOT NULL,
+                         `name` varchar(100) NOT NULL,
+                         `category` varchar(45) NOT NULL,
+                         `isAvailable` tinyint(1) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Zrzut danych tabeli `movie`
 --
 
-INSERT INTO `movie` (`id`, `name`, `category`) VALUES
-(1, 'Killing Engines', 'SCI_FI'),
-(2, 'Django', 'FANTASY'),
-(3, 'Kill Bill', 'SCI_FI'),
-(4, 'Matrix', 'SCI_FI');
+INSERT INTO `movie` (`id`, `name`, `category`, `isAvailable`) VALUES
+(1, 'Altered Carbon', 'SCI_FI', 1),
+(2, 'Django', 'FANTASY', 0),
+(3, 'Kill Bill', 'SCI_FI', 0),
+(4, 'Matrix', 'SCI_FI', 0),
+(9, 'New down', 'SCI_FI', 0);
 
 --
 -- Indeksy dla zrzutów tabel
@@ -51,7 +53,7 @@ INSERT INTO `movie` (`id`, `name`, `category`) VALUES
 -- Indeksy dla tabeli `movie`
 --
 ALTER TABLE `movie`
-  ADD PRIMARY KEY (`id`);
+    ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT dla zrzuconych tabel
@@ -61,7 +63,7 @@ ALTER TABLE `movie`
 -- AUTO_INCREMENT dla tabeli `movie`
 --
 ALTER TABLE `movie`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+    MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
